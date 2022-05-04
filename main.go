afbfe583b5de4a5bab151100d78d5f7793af7e20
walker516 <walker.f15b5b@gmail.com>
2022-05-04 17:19:56 +0900

    verify
    Signed-off-by: walker516 <walker.f15b5b@gmail.com>

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"blockchainPractice/block"
 	"blockchainPractice/wallet"
 	"fmt"
 	"log"
@@ -11,7 +12,23 @@ func init() {
 }
 
 func main() {
-	w := wallet.NewWallet()
-	fmt.Println(w.PrivateKeyStr())
-	fmt.Println(w.PublicKeyStr())
+	walletM := wallet.NewWallet()
+	walletA := wallet.NewWallet()
+	walletB := wallet.NewWallet()
+
+	// Wallet
+	t := wallet.NewTransaction(walletA.PrivateKey(), walletA.PublicKey(), walletA.BlockchainAddress(), walletB.BlockchainAddress(), 1.0)
+
+	// Blockchain
+	blockchain := block.NewBlockchain(walletM.BlockchainAddress())
+	isAdded := blockchain.AddTransaction(walletA.BlockchainAddress(), walletB.BlockchainAddress(), 1.0,
+		walletA.PublicKey(), t.GenerateSignature())
+	fmt.Println("Added? ", isAdded)
+
+	blockchain.Mining()
+	blockchain.Print()
+	fmt.Printf("A %.1f\n", blockchain.CalculateTotalAmount(walletA.BlockchainAddress()))
+	fmt.Printf("B %.1f\n", blockchain.CalculateTotalAmount(walletB.BlockchainAddress()))
+	fmt.Printf("M %.1f\n", blockchain.CalculateTotalAmount(walletM.BlockchainAddress()))
+
 }
